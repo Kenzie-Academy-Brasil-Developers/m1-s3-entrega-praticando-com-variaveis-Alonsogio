8560d650b62fc770d6834f79d5913ba2d75083ad
GiovannaAlonso <106785638+Alonsogio@users.noreply.github.com>
2022-06-14 01:36:46 -0300

    KenzieJs

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Jhon"
+let sobrenome = "Doyle"
+
+
+anoAtual = 2022
+anoNascimento = 1991
+let idade = (anoAtual - anoNascimento)
+
+let ondeMoro = "Curitiba"
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${ondeMoro}.`);
